4a8f588c61ca9eb1ee797323f14e3d531f9cb0b6
Wister <e@wister.mx>
2021-02-23 00:43:15 -0600

    Validations and hidde keyboard

--- pTweet/pTweet/Modules/Welcome/LoginViewController.swift
+++ pTweet/pTweet/Modules/Welcome/LoginViewController.swift
@@ -17,6 +17,7 @@ class LoginViewController: UIViewController {
     
     //IBacktions
     @IBAction func loginButtonAction(){
+        view.endEditing(true)
         performLogin()
     }
     
--- pTweet/pTweet/Modules/Welcome/RegisterViewController.swift
+++ pTweet/pTweet/Modules/Welcome/RegisterViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import NotificationBannerSwift
 
 class RegisterViewController: UIViewController {
+    //outlets
     @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var namesTextField: UITextField!
+    //IBacktions
+    @IBAction func registerButtonAction(){
+        performRegister()
+        view.endEditing(true)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,23 @@ class RegisterViewController: UIViewController {
     private func setupUI(){
         registerButton.layer.cornerRadius = 25
     }
-
+    private func performRegister(){
+        guard let email = emailTextField.text, !email.isEmpty else {
+            NotificationBanner(title: "Error", subtitle: "Debes Especificar un correo", style: .warning).show()
+     
+            return
+        }
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            NotificationBanner(title: "Error", subtitle: "Debes Especificar una contraseña", style: .warning).show()
+            return
+        }
+        guard let name = namesTextField.text, !name.isEmpty else {
+            NotificationBanner(title: "Error", subtitle: "Debes Especificar un nombre", style: .warning).show()
+            return
+        }
+//        if email != nil , password != nil  {
+//            NotificationBanner(title: "Success", subtitle: "Success Login", style: .success).show()
+//        }
+    }
 
 }
