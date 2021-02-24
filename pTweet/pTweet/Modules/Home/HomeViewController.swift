1d2dff5964ba3a8389d74e2ec7f9dbb5e18e4b5a
Wister <e@wister.mx>
2021-02-24 00:59:30 -0600

    Configs

--- pTweet/pTweet/Modules/Home/HomeViewController.swift
+++ pTweet/pTweet/Modules/Home/HomeViewController.swift
@@ -12,12 +12,30 @@ class HomeViewController: UIViewController {
     //mark outlets
     @IBOutlet weak var tableView: UITableView!
     
+    private let cellId = "pTweetsTableViewCell"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
         // Do any additional setup after loading the view.
     }
     
+    private func setupUI(){
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
+    }
+    
 
-
+}
+extension HomeViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        if let cell = cell as? pTweetsTableViewCell {
+            //config celda
+        }
+        return cell
+    }
 }
