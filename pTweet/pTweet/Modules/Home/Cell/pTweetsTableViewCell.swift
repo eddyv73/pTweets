bcc707e8711ee3a48299c927e4b921faf9fee0c3
Wister <e@wister.mx>
2021-02-24 00:41:56 -0600

    tweet desing

---
+++ pTweet/pTweet/Modules/Home/Cell/pTweetsTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  pTweetsTableViewCell.swift
+//  pTweet
+//
+//  Created by Eddy Wister on 24/02/21.
+//
+
+import UIKit
+import Kingfisher
+
+class pTweetsTableViewCell: UITableViewCell {
+
+    //marks
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var tweetImageView: UIImageView!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func setUpCellWith(post: Post){
+        nameLabel.text = post.author.names
+        nicknameLabel.text = post.author.nickname
+        messageLabel.text = post.text
+        if post.hasImage {
+            tweetImageView.kf.setImage(with: URL(string: post.imageUrl))
+        } else{
+            tweetImageView.isHidden = true
+        }
+    }
+    
+}
---
+++ pTweet/pTweet/Modules/Home/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  pTweet
+//
+//  Created by Eddy Wister on 24/02/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    //mark outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
