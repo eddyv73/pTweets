e438cc8033d2384475794764e66a7684c230c729
Wister <e@wister.mx>
2021-02-24 23:15:17 -0600

    addPost

---
+++ pTweet/pTweet/Modules/Home/AddPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPostViewController.swift
+//  pTweet
+//
+//  Created by Eddy Wister on 24/02/21.
+//
+
+import UIKit
+
+class AddPostViewController: UIViewController {
+
+    //marks
+    @IBOutlet weak var postTextView: UITextView!
+    @IBAction func addPostAction(){
+        
+    }
+    @IBAction func DismissAction(){
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
