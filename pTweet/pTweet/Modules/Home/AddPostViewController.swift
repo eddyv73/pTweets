6f14c7378da6bea09406d755d0e73942521802d3
Wister <e@wister.mx>
2021-02-25 23:02:29 -0600

    upload

--- pTweet/pTweet/Modules/Home/AddPostViewController.swift
+++ pTweet/pTweet/Modules/Home/AddPostViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Simple_Networking
 import SVProgressHUD
 import NotificationBannerSwift
+import FirebaseStorage
 
 
 class AddPostViewController: UIViewController {
@@ -52,7 +53,55 @@ class AddPostViewController: UIViewController {
         present(imagePicker, animated: true, completion: nil)
         
     }
+    private func Uploadphototofirebase(){
+        //marks validations
+        guard let imagesave = previewImageView.image,
+              let imageSavedData: Data = imagesave.jpegData(compressionQuality: 0.1) else {
+            return
+        }
+        
+        SVProgressHUD.show()
+        //config to save photo on firba
+        
+        let metadataCOnfig = StorageMetadata()
+        metadataCOnfig.contentType = "image/jpg"
+        
+        //create referece
+        let storage = Storage.storage()
+        //set name to image
+        
+        let imageNme = Int.random(in: 100...1000)
+        
+        //referencecarpet
+        
+        let folderReference = storage.reference(withPath: "photos-of-ptweets/\(imageNme).jpg")
+        
+        //uploadphoto
+        
+        DispatchQueue.global(qos: .background).async {
+            folderReference.putData(imageSavedData, metadata: metadataCOnfig){
+                (metaData: StorageMetadata? , error: Error?)in
+                DispatchQueue.main.async {
+                    //detener carga
+                    SVProgressHUD.dismiss()
+                    if let error = error{
+                        NotificationBanner(subtitle: error.localizedDescription , style: .warning).show()
+                        return
+                    }
+                    
+                    //obtener url
+                    folderReference.downloadURL { (url : URL?, error: Error?) in
+                        print(url?.absoluteString ?? "")
+                    }
+                }
+            }
+        }
+        
+        
+    }
     private func savePost(){
+        Uploadphototofirebase()
+        return
         let request = PostRequest(text: postTextView.text, imageUrl: nil, videoUrl: nil, location: nil)
         SVProgressHUD.show()
         
